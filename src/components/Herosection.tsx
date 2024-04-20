9980873f41c22ffd324c66953d5eec0b9ed43d02
Ram <67099162+Ram0306@users.noreply.github.com>
2024-04-20 17:31:05 +0530

    Update Herosection.tsx   &ldquo;

--- src/components/Herosection.tsx
+++ src/components/Herosection.tsx
@@ -15,7 +15,7 @@ function Herosection() {
             <h1 className='mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-50'>
               Master the art of music</h1>
             <p className='mt-5 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto'>
-"Master the Art of Music" is a journey into the depths of musical craftsmanship, a comprehensive guide designed to empower musicians of all levels. This book delves into the nuances of music theory, performance techniques, composition strategies, and the rich history of various musical genres.</p>
+  &ldquo;Master the Art of Music  &ldquo; is a journey into the depths of musical craftsmanship, a comprehensive guide designed to empower musicians of all levels. This book delves into the nuances of music theory, performance techniques, composition strategies, and the rich history of various musical genres.</p>
                  <div className='mt-4'>
                     <Link href={"/courses"}>
                         <Button borderRadius="1.75rem" 
