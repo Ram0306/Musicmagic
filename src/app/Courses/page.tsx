a0e7fbb5af95f1c3468fab31db7852be7873b139
Ram <67099162+Ram0306@users.noreply.github.com>
2024-04-20 17:29:57 +0530

    update key   key ={course.id}

--- src/app/Courses/page.tsx
+++ src/app/Courses/page.tsx
@@ -13,6 +13,8 @@ function page(){
       <CardContainer className="inter-var">
         <CardBody className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-[2rem] p-5 border ">
           <CardItem
+            key ={course.id}
+
             translateZ="50"
             className="text-xl font-bold  dark:text-white"
           >
