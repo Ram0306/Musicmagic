70fec07ece35d1fdc886b8deda05b5164e0d7114
Ram <67099162+Ram0306@users.noreply.github.com>
2024-04-20 17:45:36 +0530

    Update page.tsx     key ={course.id}

--- src/app/Courses/page.tsx
+++ src/app/Courses/page.tsx
@@ -10,10 +10,10 @@ function page(){
         <h1 className="text-lg md:text-7xl text-center font-sans font-bold mb-8"> All courses {Coursedata.courses.length}</h1>
         <div className="flex flex-wrap  gap-6 justify-center">
             {Coursedata.courses.map((course)=>(
-      <CardContainer className="inter-var">
+      <CardContainer className="inter-var"   key ={course.id}>
         <CardBody className="bg-black relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-[2rem] p-5 border ">
           <CardItem
-            key ={course.id}
+          
 
             translateZ="50"
             className="text-xl font-bold  dark:text-white"
